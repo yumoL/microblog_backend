b826a732538fcb20637f3c3e6f7c76e86c2e5969
luoyumo <yumo.luo@hotmail.com>
2020-01-16 14:37:31 +0200

    print database name

--- database/models/index.js
+++ database/models/index.js
@@ -15,6 +15,7 @@ if (config.use_env_variable) {
 } else {
   console.log('no env')
   sequelize = new Sequelize(config.database, config.username, config.password, config)
+  console.log(config.database)
 }
 
 fs
