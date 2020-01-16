8ca679a3cea146896760c242d152b1615033a438
luoyumo <yumo.luo@hotmail.com>
2020-01-16 14:50:43 +0200

    template string

--- database/config/config.js
+++ database/config/config.js
@@ -18,10 +18,10 @@ module.exports = {
     operatorsAlises: false
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    username: `${process.env.DB_USER}`,
+    password: `${process.env.DB_PASSWORD}`,
+    database: `${process.env.DB_NAME}`,
+    host: `${process.env.DB_HOST}`,
     dialect: 'mysql',
     operatorsAlises: false
   }
